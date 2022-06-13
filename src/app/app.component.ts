0f14dd71beaf48b2bde9aad688af27dbd044e089
keifermadness <keifergilbert@gmail.com>
2022-06-13 14:13:08 -0700

    added logic to get to hidden pages in pagination navigator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,5 +26,41 @@ export class AppComponent {
       title: 'In a Sewer',
       url: 'https://images.unsplash.com/photo-1530424257364-cd52a3e9bd40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2V3ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
     },
-  ];
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+      title: 'In the Mountains',
+      url: 'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+      title: 'At the Lake',
+      url: 'https://images.unsplash.com/photo-1439066615861-d1af74d74000?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFrZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+      title: 'In a Sewer',
+      url: 'https://images.unsplash.com/photo-1530424257364-cd52a3e9bd40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2V3ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+      title: 'In the Mountains',
+      url: 'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+      title: 'At the Lake',
+      url: 'https://images.unsplash.com/photo-1439066615861-d1af74d74000?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFrZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+      title: 'In a Sewer',
+      url: 'https://images.unsplash.com/photo-1530424257364-cd52a3e9bd40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2V3ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+    },
+  ]
+
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) <5;
+  }
 }
